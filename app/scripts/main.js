43769b7429c683b2a82c7854b79dcd135b75fcf8
haiman232 <haiman.jp@gmail.com>
2021-03-22 14:30:40 +0900

    Slick slider and bring object to mixin pug, sass

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,3 +29,23 @@ window.addEventListener("click", function (e) {
     menuHeader.classList.remove(expandClass);
   }
 });
+
+//Slick slider
+$(document).ready(function () {
+  $(".quote-container").slick({
+    // autoplay: true,
+    // arrows: true,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
